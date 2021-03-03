7bbced5becc20cffdad28eceb0cfac049d4a2f09
Nick Gheorghita <nickgheorghita@gmail.com>
2021-03-03 12:22:06 +0000

    Add some tests for cli arg parsing

--- src/cli.rs
+++ src/cli.rs
@@ -8,19 +8,25 @@ use std::net::TcpStream;
 use std::os::unix;
 use threadpool::ThreadPool;
 
-pub fn launch_trin(infura_project_id: String, protocol: String) {
-    println!("Launching with infura key: '{}'", infura_project_id);
+#[derive(Debug, PartialEq)]
+pub struct TrinConfig {
+    pub protocol: String,
+    pub infura_project_id: String,
+    pub endpoint: u32,
+    pub pool_size: u32,
+}
 
-    let pool = ThreadPool::new(2);
+pub fn launch_trin(trin_config: TrinConfig) {
+    let pool = ThreadPool::new(trin_config.pool_size as usize);
 
-    match &protocol[..] {
-        "ipc" => launch_ipc_client(pool, infura_project_id),
-        "http" => launch_http_client(pool, infura_project_id),
-        _ => panic!("invalid"),
+    match &trin_config.protocol[..] {
+        "ipc" => launch_ipc_client(pool, trin_config),
+        "http" => launch_http_client(pool, trin_config),
+        val => panic!("Unsupported protocol: {}", val),
     }
 }
 
-fn launch_ipc_client(pool: ThreadPool, infura_project_id: String) {
+fn launch_ipc_client(pool: ThreadPool, trin_config: TrinConfig) {
     let path = "/tmp/trin-jsonrpc.ipc";
     let listener_result = unix::net::UnixListener::bind(path);
     let listener = match listener_result {
@@ -40,85 +46,13 @@ fn launch_ipc_client(pool: ThreadPool, infura_project_id: String) {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let infura_project_id = infura_project_id.clone();
+        let infura_project_id = trin_config.infura_project_id.clone();
         pool.execute(move || {
             let infura_url = format!("https://mainnet.infura.io:443/v3/{}", infura_project_id);
             let mut rx = stream.try_clone().unwrap();
             let mut tx = stream;
             serve_ipc_client(&mut rx, &mut tx, &infura_url);
         });
-    }
-}
-
-fn launch_http_client(pool: ThreadPool, infura_project_id: String) {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        let infura_project_id = infura_project_id.clone();
-        pool.execute(move || {
-            let infura_url = format!("https://mainnet.infura.io:443/v3/{}", infura_project_id);
-            serve_http_client(stream, &infura_url);
-        });
-    }
-}
-
-fn serve_http_client(mut stream: TcpStream, infura_url: &String) {
-    let mut buffer = [0; 1024];
-
-    stream.read(&mut buffer).unwrap();
-
-    let request = String::from_utf8_lossy(&buffer[..]);
-    let json_request = match request.lines().last() {
-        None => panic!("failed"),
-        Some(last_line) => last_line.split('\u{0}').nth(0).unwrap(),
-    };
-
-    let deser = serde_json::Deserializer::from_str(&json_request);
-    for obj in deser.into_iter::<serde_json::Value>() {
-        let obj = obj.unwrap();
-        assert!(obj.is_object());
-        assert_eq!(obj["jsonrpc"], "2.0");
-        let request_id = obj.get("id").unwrap();
-        let method = obj.get("method").unwrap();
-
-        let response = match method.as_str().unwrap() {
-            "web3_clientVersion" => {
-                let contents = format!(
-                    r#"{{"jsonrpc":"2.0","id":{},"result":"trin 0.0.1-alpha"}}"#,
-                    request_id
-                );
-                format!(
-                    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                    contents.len(),
-                    contents,
-                )
-                .into_bytes()
-            }
-            _ => {
-                //Re-encode json to proxy to Infura
-                let request = obj.to_string();
-                match proxy_to_url(request, infura_url) {
-                    // test infura error handling?
-                    Ok(result_body) => {
-                        let contents = String::from_utf8_lossy(&result_body);
-                        format!(
-                            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                            contents.len(),
-                            contents,
-                        )
-                        .into_bytes()
-                    }
-                    Err(err) => format!(
-                        r#"{{"jsonrpc":"2.0","id":"{}","error":"Infura failure: {}"}}"#,
-                        request_id,
-                        err.to_string(),
-                    )
-                    .into_bytes(),
-                }
-            }
-        };
-        stream.write(&response).unwrap();
-        stream.flush().unwrap();
     }
 }
 
@@ -155,6 +89,92 @@ fn serve_ipc_client(rx: &mut impl Read, tx: &mut impl Write, infura_url: &String
         tx.write_all(&response).unwrap();
     }
     println!("Clean exit");
+}
+
+fn launch_http_client(pool: ThreadPool, trin_config: TrinConfig) {
+    let uri = format!("127.0.0.1:{}", trin_config.endpoint);
+    let listener = TcpListener::bind(uri).unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let infura_project_id = trin_config.infura_project_id.clone();
+                pool.execute(move || {
+                    let infura_url =
+                        format!("https://mainnet.infura.io:443/v3/{}", infura_project_id);
+                    serve_http_client(stream, &infura_url);
+                });
+            }
+            Err(e) => {
+                panic!("HTTP connection failed: {}", e)
+            }
+        }
+    }
+}
+
+fn serve_http_client(mut stream: TcpStream, infura_url: &String) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let json_request = match request.lines().last() {
+        None => panic!("Invalid json request."),
+        Some(last_line) => last_line.split('\u{0}').nth(0).unwrap(),
+    };
+
+    let deser = serde_json::Deserializer::from_str(&json_request);
+    for obj in deser.into_iter::<serde_json::Value>() {
+        let obj = obj.unwrap();
+        assert!(obj.is_object());
+        assert_eq!(obj["jsonrpc"], "2.0");
+        let request_id = obj.get("id").unwrap();
+        let method = obj.get("method").unwrap();
+
+        let response = match method.as_str().unwrap() {
+            "web3_clientVersion" => {
+                let contents = format!(
+                    r#"{{"jsonrpc":"2.0","id":{},"result":"trin 0.0.1-alpha"}}"#,
+                    request_id
+                );
+                format!(
+                    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                    contents.len(),
+                    contents,
+                )
+                .into_bytes()
+            }
+            _ => {
+                //Re-encode json to proxy to Infura
+                let request = obj.to_string();
+                match proxy_to_url(request, infura_url) {
+                    Ok(result_body) => {
+                        let contents = String::from_utf8_lossy(&result_body);
+                        format!(
+                            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                            contents.len(),
+                            contents,
+                        )
+                        .into_bytes()
+                    }
+                    Err(err) => {
+                        let contents = format!(
+                            r#"{{"jsonrpc":"2.0","id":"{}","error":"Infura failure: {}"}}"#,
+                            request_id,
+                            err.to_string(),
+                        );
+                        format!(
+                            "HTTP/1.1 502 BAD GATEWAY\r\nContent-Length: {}\r\n\r\n{}",
+                            contents.len(),
+                            contents,
+                        )
+                        .into_bytes()
+                    }
+                }
+            }
+        };
+        stream.write(&response).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 fn proxy_to_url(request: String, url: &String) -> io::Result<Vec<u8>> {
