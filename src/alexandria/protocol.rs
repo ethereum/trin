a66fa3e465763176869e9df04136fcde2e46ca0a
Sam Wilson <sam.wilson@mesh.xyz>
2021-05-05 14:52:02 -0400

    Use new TalkRequest type

--- src/alexandria/protocol.rs
+++ src/alexandria/protocol.rs
@@ -6,7 +6,7 @@ use super::{
     U256,
 };
 use super::{types::Message, Enr};
-use discv5::{Discv5ConfigBuilder, TalkReqHandler};
+use discv5::{Discv5ConfigBuilder, TalkReqHandler, TalkRequest};
 use log::warn;
 use tokio::sync::mpsc;
 
@@ -15,43 +15,18 @@ pub const PROTOCOL: &str = "state-network";
 pub struct AlexandriaProtocol {
     discovery: Discovery,
     data_radius: U256,
-    protocol_receiver: mpsc::UnboundedReceiver<(Request, mpsc::Sender<Result<Response, String>>)>,
+    protocol_receiver: mpsc::UnboundedReceiver<TalkRequest>,
 }
 
 #[derive(Clone)]
 pub struct ProtocolHandler {
-    protocol_sender: mpsc::UnboundedSender<(Request, mpsc::Sender<Result<Response, String>>)>,
+    protocol_sender: mpsc::UnboundedSender<TalkRequest>,
 }
 
 impl TalkReqHandler for ProtocolHandler {
-    fn talkreq_response(&self, protocol: &[u8], req: &[u8]) -> Vec<u8> {
-        if let Ok(protocol) = String::from_utf8(protocol.to_vec()) {
-            if protocol != PROTOCOL {
-                return "Invalid protocol".as_bytes().to_vec();
-            }
-            match Message::from_bytes(req) {
-                Err(e) => format!("Invalid request: {}", e).as_bytes().to_vec(),
-                Ok(msg) => {
-                    if let Message::Request(req) = msg {
-                        let (tx, mut rx) = mpsc::channel(1);
-                        if let Err(_) = self.protocol_sender.send((req, tx)) {
-                            return "Internal error".as_bytes().to_vec();
-                        }
-                        let resp = std::thread::spawn(move || match rx.blocking_recv() {
-                            None => "Internal error".as_bytes().to_vec(),
-                            Some(r) => match r {
-                                Ok(resp) => Message::Response(resp).to_bytes(),
-                                Err(e) => format!("Internal error {}", e).as_bytes().to_vec(),
-                            },
-                        });
-                        resp.join().unwrap()
-                    } else {
-                        "Invalid message".as_bytes().to_vec()
-                    }
-                }
-            }
-        } else {
-            "Invalid protocol".as_bytes().to_vec()
+    fn talkreq_response(&self, request: TalkRequest) {
+        if self.protocol_sender.send(request).is_err() {
+            warn!("sending on disconnected channel");
         }
     }
 }
@@ -118,34 +93,55 @@ impl AlexandriaProtocol {
     /// Receives a request from the talkreq handler and sends a response back
     pub fn process_request(mut self, handle: tokio::runtime::Handle) {
         let fut = async move {
-            while let Some((request, channel)) = self.protocol_receiver.recv().await {
-                let response = match request {
-                    Request::Ping(Ping { .. }) => {
-                        let enr_seq = self.discovery.local_enr().seq();
-                        Response::Pong(Pong {
-                            enr_seq: enr_seq as u32,
-                            data_radius: self.data_radius,
-                        })
-                    }
-
-                    Request::FindNodes(FindNodes { distances }) => {
-                        let enrs = self.discovery.find_nodes_response(distances);
-                        Response::Nodes(Nodes {
-                            total: enrs.len() as u8,
-                            enrs,
-                        })
-                    }
-                    // TODO
-                    Request::FindContent(FindContent { .. }) => {
-                        Response::FoundContent(FoundContent { enrs: vec![] })
-                    }
+            while let Some(request) = self.protocol_receiver.recv().await {
+                let reply = match self.process_one_request(&request).await {
+                    Ok(r) => Message::Response(r).to_bytes(),
+                    Err(e) => e.into_bytes(),
                 };
-                if let Err(e) = channel.send(Ok(response)).await {
-                    warn!("Failed to respond to talkreq channel: {}", e);
-                }
+
+                request.respond(reply);
             }
         };
 
         handle.spawn(fut);
+    }
+
+    async fn process_one_request(&self, talk_request: &TalkRequest) -> Result<Response, String> {
+        let protocol = std::str::from_utf8(talk_request.protocol())
+            .map_err(|_| "Invalid protocol".to_owned())?;
+
+        if protocol != PROTOCOL {
+            return Err("Invalid protocol".to_owned());
+        }
+
+        let request = match Message::from_bytes(talk_request.body()) {
+            Ok(Message::Request(r)) => r,
+            Ok(_) => return Err("Invalid message".to_owned()),
+            Err(e) => return Err(format!("Invalid request: {}", e)),
+        };
+
+        let response = match request {
+            Request::Ping(Ping { .. }) => {
+                let enr_seq = self.discovery.local_enr().seq();
+                Response::Pong(Pong {
+                    enr_seq: enr_seq as u32,
+                    data_radius: self.data_radius,
+                })
+            }
+
+            Request::FindNodes(FindNodes { distances }) => {
+                let enrs = self.discovery.find_nodes_response(distances);
+                Response::Nodes(Nodes {
+                    total: enrs.len() as u8,
+                    enrs,
+                })
+            }
+            // TODO
+            Request::FindContent(FindContent { .. }) => {
+                Response::FoundContent(FoundContent { enrs: vec![] })
+            }
+        };
+
+        Ok(response)
     }
 }
