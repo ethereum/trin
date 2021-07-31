0b3852863e740ceb8232d20b32eafd352e019076
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2021-07-30 22:56:29 -0600

    Create workspace for multiple networks (#54)
    * Create workspace for multiple networks
    
    - Create individual packages for the state and history networks.
    - Move the main() function from main.rs to lib.rs for the state network.
    - Remove main.rs for the state nework.
    - Add a basic main() function in the history network lib.rs.
    - Create a 'trin' package to host a single entry point for the program.
    
    * Update for 'cargo fmt'
    
    * Fix failing lint-build-test and update README.md
    
    * Fix merge conflicts in utils.rs and Cargo.toml
    
    * Minor fixes to README and Cargo.toml
    
    Co-authored-by: mrferris <mrferris@buffalo.edu>

---
+++ trin/src/main.rs
@@ -0,0 +1,11 @@
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tokio::select! {
+        history = trin_history::main() => {
+            history
+        },
+        state = trin_state::main() => {
+            state
+        },
+    }
+}
