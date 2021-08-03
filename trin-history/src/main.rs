9afdcc7de9d6825eefc26bc7051e4b34cbb74347
mrferris <mrferris@buffalo.edu>
2021-08-02 22:56:18 -0400

    Updates src/main.rs to be more idiomatic

--- trin-history/src/main.rs
+++ trin-history/src/main.rs
@@ -2,5 +2,5 @@ use trin_history;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    return trin_history::main().await;
+    trin_history::main().await
 }
--- trin-state/src/main.rs
+++ trin-state/src/main.rs
@@ -2,5 +2,5 @@ use trin_state;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    return trin_state::main().await;
+    trin_state::main().await
 }
