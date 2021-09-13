d227ae41bd76788c933f97ef0a0846f679f014f9
Nick Gheorghita <nickgheorghita@gmail.com>
2021-09-13 18:18:51 -0400

    Update distance argument types to U256

--- trin-state/src/utils.rs
+++ trin-state/src/utils.rs
@@ -1,5 +1,6 @@
 use num::bigint::{BigInt, Sign};
 use num::Signed;
+use trin_core::portalnet::U256;
 
 // 2 ** 256
 const MODULO: [u8; 78] = [
@@ -17,12 +18,39 @@ const MID: [u8; 77] = [
     49, 57, 57, 54, 56,
 ];
 
+trait FromU256 {
+    fn from_u256(n: U256) -> Self;
+}
+
+impl FromU256 for BigInt {
+    fn from_u256(n: U256) -> Self {
+        let mut bytes: [u8; 32] = [0u8; 32];
+        n.to_little_endian(&mut bytes);
+        BigInt::from_bytes_le(Sign::Plus, &bytes)
+    }
+}
+
+trait ToU256 {
+    fn to_u256(&self) -> Result<U256, String>;
+}
+
+impl ToU256 for BigInt {
+    fn to_u256(&self) -> Result<U256, String> {
+        let (sign, bytes) = self.to_bytes_be();
+        match sign {
+            Sign::Plus => Ok(U256::from_big_endian(&bytes)),
+            Sign::Minus => Err("Cannot convert negative BigInt to unsigned int".to_string()),
+            Sign::NoSign => Ok(U256::from_big_endian(&bytes)),
+        }
+    }
+}
+
 // distance function as defined at...
 // https://notes.ethereum.org/h58LZcqqRRuarxx4etOnGQ#Storage-Layout
 // todo: measure performance & optimize if necessary
-pub fn distance(node_id: [u8; 32], content_id: [u8; 32]) -> BigInt {
-    let node_id_int = BigInt::parse_bytes(&node_id, 10).unwrap();
-    let content_id_int = BigInt::parse_bytes(&content_id, 10).unwrap();
+pub fn distance(node_id: U256, content_id: U256) -> Result<U256, String> {
+    let node_id_int = BigInt::from_u256(node_id);
+    let content_id_int = BigInt::from_u256(content_id);
     let modulo = BigInt::parse_bytes(&MODULO, 10).unwrap();
     let mid = BigInt::parse_bytes(&MID, 10).unwrap();
     let negative_mid = mid
@@ -34,8 +62,8 @@ pub fn distance(node_id: [u8; 32], content_id: [u8; 32]) -> BigInt {
     let modulus_result = ((first_phrase % &modulo) + &modulo) % &modulo;
     let delta = modulus_result - &mid;
     match delta < negative_mid {
-        true => (delta + mid).abs(),
-        _ => delta.abs(),
+        true => (delta + mid).abs().to_u256(),
+        _ => delta.abs().to_u256(),
     }
 }
 
@@ -44,77 +72,83 @@ mod test {
     use super::*;
 
     // 2 ** 256 - 1
-    const MOD_SUB_ONE: &[u8] =
-        "115792089237316195423570985008687907853269984665640564039457584007913129639935".as_bytes();
+    const MOD_SUB_ONE: &str =
+        "115792089237316195423570985008687907853269984665640564039457584007913129639935";
+    // 2 ** 255
+    const MID_DEC_STR: &str =
+        "57896044618658097711785492504343953926634992332820282019728792003956564819968";
 
-    // test cases yanked from: https://notes.ethereum.org/h58LZcqqRRuarxx4etOnGQ
+    #[test]
+    fn test_number_conversion() {
+        let number = "10";
+        let u_256 = U256::from_dec_str(number).unwrap();
+        let big_int = BigInt::from_u256(u_256);
+        let next = big_int.to_u256().unwrap();
+        assert_eq!(next, u_256)
+    }
+
+    //test cases yanked from: https://notes.ethereum.org/h58LZcqqRRuarxx4etOnGQ
     #[test]
     fn test_distance_with_zeros() {
-        let mut content_id: [u8; 32] = Default::default();
-        content_id.copy_from_slice("10".as_bytes());
-        let mut node_id: [u8; 32] = Default::default();
-        node_id.copy_from_slice("10".as_bytes());
-        let expected = BigInt::parse_bytes("0".as_bytes(), 10).unwrap();
-        let calculated_distance = distance(content_id, node_id);
+        let content_id = U256::from_dec_str("10").unwrap();
+        let node_id = U256::from_dec_str("10").unwrap();
+        let expected = U256::from_dec_str("0").unwrap();
+        let calculated_distance = distance(content_id, node_id).unwrap();
         assert_eq!(calculated_distance, expected);
     }
 
     #[test]
     fn test_distance_one() {
-        let content_id = BigInt::parse_bytes("5".as_bytes(), 10).unwrap();
-        let node_id = BigInt::parse_bytes("1".as_bytes(), 10).unwrap();
-        let expected = BigInt::parse_bytes("4".as_bytes(), 10).unwrap();
-        let calculated_distance = distance(content_id.clone(), node_id.clone());
+        let content_id = U256::from_dec_str("5").unwrap();
+        let node_id = U256::from_dec_str("1").unwrap();
+        let expected = U256::from_dec_str("4").unwrap();
+        let calculated_distance = distance(content_id.clone(), node_id.clone()).unwrap();
         assert_eq!(calculated_distance, expected);
-        let reversed = distance(node_id, content_id);
+        let reversed = distance(node_id, content_id).unwrap();
         assert_eq!(reversed, expected);
     }
 
     #[test]
     fn test_distance_two() {
-        let content_id = BigInt::parse_bytes("5".as_bytes(), 10).unwrap();
-        let node_id = BigInt::parse_bytes(MOD_SUB_ONE, 10).unwrap();
-        let expected = BigInt::parse_bytes("6".as_bytes(), 10).unwrap();
-        let calculated_distance = distance(content_id, node_id);
+        let content_id = U256::from_dec_str("5").unwrap();
+        let node_id = U256::from_dec_str(MOD_SUB_ONE).unwrap();
+        let expected = U256::from_dec_str("6").unwrap();
+        let calculated_distance = distance(content_id, node_id).unwrap();
         assert_eq!(calculated_distance, expected);
     }
 
     #[test]
     fn test_distance_three() {
-        let content_id = BigInt::parse_bytes(MOD_SUB_ONE, 10).unwrap();
-        let node_id = BigInt::parse_bytes("6".as_bytes(), 10).unwrap();
-        let expected = BigInt::parse_bytes("7".as_bytes(), 10).unwrap();
-        let calculated_distance = distance(content_id, node_id);
+        let content_id = U256::from_dec_str(MOD_SUB_ONE).unwrap();
+        let node_id = U256::from_dec_str("6").unwrap();
+        let expected = U256::from_dec_str("7").unwrap();
+        let calculated_distance = distance(content_id, node_id).unwrap();
         assert_eq!(calculated_distance, expected);
     }
 
     #[test]
     fn test_distance_four() {
-        let content_id = BigInt::parse_bytes("0".as_bytes(), 10).unwrap();
-        let node_id = BigInt::parse_bytes(&MID, 10).unwrap();
-        let expected = BigInt::parse_bytes(&MID, 10).unwrap();
-        let calculated_distance = distance(content_id, node_id);
+        let content_id = U256::from_dec_str("0").unwrap();
+        let node_id = U256::from_dec_str(&MID_DEC_STR).unwrap();
+        let expected = U256::from_dec_str(&MID_DEC_STR).unwrap();
+        let calculated_distance = distance(content_id, node_id).unwrap();
         assert_eq!(calculated_distance, expected);
     }
 
     #[test]
     fn test_distance_five() {
-        let content_id = BigInt::parse_bytes("0".as_bytes(), 10).unwrap();
+        let content_id = U256::from_dec_str("0").unwrap();
         // 2 ** 255 + 1
-        let node_id = BigInt::parse_bytes(
-            "57896044618658097711785492504343953926634992332820282019728792003956564819969"
-                .as_bytes(),
-            10,
+        let node_id = U256::from_dec_str(
+            "57896044618658097711785492504343953926634992332820282019728792003956564819969",
         )
         .unwrap();
         // 2 ** 255 - 1
-        let expected = BigInt::parse_bytes(
-            "57896044618658097711785492504343953926634992332820282019728792003956564819967"
-                .as_bytes(),
-            10,
+        let expected = U256::from_dec_str(
+            "57896044618658097711785492504343953926634992332820282019728792003956564819967",
         )
         .unwrap();
-        let calculated_distance = distance(content_id, node_id);
+        let calculated_distance = distance(content_id, node_id).unwrap();
         assert_eq!(calculated_distance, expected);
     }
 }
