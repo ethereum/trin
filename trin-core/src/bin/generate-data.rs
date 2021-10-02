e078f9853cb4d461a6e60068db524c56c16abe16
mrferris <mrferris@buffalo.edu>
2021-10-02 14:22:44 -0400

    another clippy fix

--- trin-core/src/bin/generate-data.rs
+++ trin-core/src/bin/generate-data.rs
@@ -44,7 +44,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         db: Arc::new(db),
         meta_db: Arc::new(meta_db),
     };
-    let mut storage = PortalStorage::new(storage_config, |key| sha256(&key))?;
+    let mut storage = PortalStorage::new(storage_config, |key| sha256(key))?;
 
     for _ in 0..num_of_entries {
         let value = generate_random_value(size_of_values);
