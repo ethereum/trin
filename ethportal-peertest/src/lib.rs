03d85bb51b29dafe1addce2fe90141a6230888e5
Jason Carver <ut96caarrs@snkmail.com>
2021-11-19 13:01:54 -0800

    Run ethportal_peertest on launched trin client
    This must run as multithreaded, or the event loop blocks and IPC
    requests will not be handled.

--- ethportal-peertest/src/lib.rs
+++ ethportal-peertest/src/lib.rs
@@ -1,3 +1,7 @@
 pub mod cli;
 pub mod events;
 pub mod jsonrpc;
+
+pub use jsonrpc::{test_jsonrpc_endpoints_over_http, test_jsonrpc_endpoints_over_ipc};
+
+pub use cli::PeertestConfig;
--- tests/self_peertest.rs
+++ tests/self_peertest.rs
@@ -1,16 +1,28 @@
 #[cfg(test)]
 mod test {
-    use tokio::time::{sleep, Duration};
+    use ethportal_peertest as peertest;
     use trin_core::cli::TrinConfig;
 
-    #[tokio::test]
+    #[tokio::test(flavor = "multi_thread")]
     async fn test_launches() {
-        let trin_config = TrinConfig::new_from(["trin"].iter()).unwrap();
+        tracing_subscriber::fmt::init();
+
+        let peertest_config = peertest::PeertestConfig::new_from([".", "--target-node", "enr:-IS4QFuwD0Zgt_R_yRbIMHHQq_iAmewBewahQgIjIiSTRnzZWp_y8atv0jSN0oeE3KJ8qnNHI-AEgfJfqWYVEqvjWqYBgmlkgnY0gmlwhMCoAcCJc2VjcDI1NmsxoQI19BFyTJb6NhSYOi3nJQJdXL3WESUI4ouxYzBr423MdIN1ZHCCIyg"].iter()).unwrap();
+
+        let trin_config = TrinConfig::new_from(
+            [
+                "trin",
+                "--internal-ip",
+                "--web3-ipc-path",
+                &peertest_config.web3_ipc_path,
+            ]
+            .iter(),
+        )
+        .unwrap();
         let exiter = trin::run_trin(trin_config, String::new()).await.unwrap();
 
-        sleep(Duration::from_millis(1000)).await;
+        peertest::test_jsonrpc_endpoints_over_ipc(peertest_config).await;
 
-        // TODO run ethportal_peertest against the ENR
         exiter.exit();
     }
 }
