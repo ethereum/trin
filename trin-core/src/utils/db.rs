5855747be75b6058f77921041a4ec7d7ef682c20
Nick Gheorghita <nickgheorghita@gmail.com>
2022-04-05 14:25:05 -0400

    Fix data dir setup

--- trin-core/src/utils/db.rs
+++ trin-core/src/utils/db.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use std::{env, fs};
 
 use directories::ProjectDirs;
@@ -6,16 +7,8 @@ use discv5::enr::NodeId;
 const TRIN_DATA_ENV_VAR: &str = "TRIN_DATA_PATH";
 
 pub fn get_data_dir(node_id: NodeId) -> String {
-    let path = env::var(TRIN_DATA_ENV_VAR).unwrap_or_else(|_| get_default_data_dir(node_id));
-
-    fs::create_dir_all(&path).expect("Unable to create data directory folder");
-    path
-}
-
-pub fn get_default_data_dir(node_id: NodeId) -> String {
-    // Windows: C:\Users\Username\AppData\Roaming\Trin\data
-    // macOS: ~/Library/Application Support/Trin
-    // Unix-like: $HOME/.local/share/trin
+    let trin_data_dir = env::var(TRIN_DATA_ENV_VAR).unwrap_or_else(|_| get_default_data_dir());
+    let trin_data_dir = Path::new(&trin_data_dir);
 
     // Append first 8 characters of Node ID
     let mut application_string = "Trin_".to_owned();
@@ -23,7 +16,20 @@ pub fn get_default_data_dir(node_id: NodeId) -> String {
     let suffix = &node_id_string[..8];
     application_string.push_str(suffix);
 
-    match ProjectDirs::from("", "", &application_string) {
+    let node_id_data_dir = trin_data_dir
+        .join(application_string)
+        .to_str()
+        .unwrap()
+        .to_string();
+    fs::create_dir_all(&node_id_data_dir).expect("Unable to create data directory folder");
+    node_id_data_dir
+}
+
+pub fn get_default_data_dir() -> String {
+    // Windows: C:\Users\Username\AppData\Roaming\trin
+    // macOS: ~/Library/Application Support/trin
+    // Unix-like: $HOME/.local/share/trin
+    match ProjectDirs::from("", "", "trin") {
         Some(proj_dirs) => proj_dirs.data_local_dir().to_str().unwrap().to_string(),
         None => panic!("Unable to find data directory"),
     }
