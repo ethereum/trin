96e89ba1834529a2fc4539fe2112c545ee9ddce7
Ognyan Genev <ognian.genev@gmail.com>
2023-01-23 14:22:27 +0200

    trin-state: Remove support for state network json-rpc endpoints

--- trin-state/src/jsonrpc.rs
+++ trin-state/src/jsonrpc.rs
@@ -1,22 +1,10 @@
 use std::sync::Arc;
 
-use serde_json::Value;
 use tokio::sync::mpsc;
-use tracing::error;
+use tracing::warn;
 
 use crate::network::StateNetwork;
-use trin_core::utils::bytes::hex_encode;
-use trin_core::{
-    jsonrpc::{
-        endpoints::StateEndpoint,
-        types::{
-            FindContentParams, FindNodesParams, LocalContentParams, PingParams,
-            RecursiveFindContentParams, SendOfferParams, StateJsonRpcRequest, StoreParams,
-        },
-        utils::bucket_entries_to_json,
-    },
-    portalnet::{storage::ContentStore, types::content_key::StateContentKey},
-};
+use trin_core::jsonrpc::types::StateJsonRpcRequest;
 
 /// Handles State network JSON-RPC requests
 pub struct StateRequestHandler {
@@ -26,170 +14,8 @@ pub struct StateRequestHandler {
 
 impl StateRequestHandler {
     pub async fn handle_client_queries(mut self) {
-        while let Some(request) = self.state_rx.recv().await {
-            match request.endpoint {
-                StateEndpoint::LocalContent => {
-                    let response =
-                        match LocalContentParams::<StateContentKey>::try_from(request.params) {
-                            Ok(params) => {
-                                match &self.network.overlay.store.read().get(&params.content_key) {
-                                    Ok(val) => match val {
-                                        Some(val) => Ok(Value::String(hex_encode(val.clone()))),
-                                        None => Ok(Value::String("0x0".to_string())),
-                                    },
-                                    Err(_) => Err(format!(
-                                        "Unable to find content key in local storage: {:?}",
-                                        params.content_key
-                                    )),
-                                }
-                            }
-                            Err(msg) => Err(format!("Invalid LocalContent params: {msg:?}")),
-                        };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::Store => {
-                    let response = match StoreParams::<StateContentKey>::try_from(request.params) {
-                        Ok(params) => {
-                            let content_key = params.content_key.clone();
-                            let content = params.content.clone();
-                            match self
-                                .network
-                                .overlay
-                                .store
-                                .write()
-                                .put(content_key, &content)
-                            {
-                                Ok(_) => Ok(Value::Bool(true)),
-                                Err(msg) => Ok(Value::String(msg.to_string())),
-                            }
-                        }
-                        Err(msg) => Ok(Value::String(msg.to_string())),
-                    };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::RecursiveFindContent => {
-                    let find_content_params =
-                        match RecursiveFindContentParams::try_from(request.params) {
-                            Ok(params) => params,
-                            Err(msg) => {
-                                let _ = request.resp.send(Err(format!(
-                                    "Invalid RecursiveFindContent params: {:?}",
-                                    msg.code
-                                )));
-                                return;
-                            }
-                        };
-
-                    let response =
-                        match StateContentKey::try_from(find_content_params.content_key.to_vec()) {
-                            Ok(content_key) => {
-                                // Check whether we have the data locally.
-                                let local_content: Option<Vec<u8>> =
-                                    match self.network.overlay.store.read().get(&content_key) {
-                                        Ok(Some(data)) => Some(data),
-                                        Ok(None) => None,
-                                        Err(err) => {
-                                            error!(
-                                                error = %err,
-                                                content.key = %content_key,
-                                                "Error checking data store for content",
-                                            );
-                                            None
-                                        }
-                                    };
-                                match local_content {
-                                    None => {
-                                        match self.network.overlay.lookup_content(content_key).await
-                                        {
-                                            (Some(content), _) => {
-                                                let value = Value::String(hex_encode(content));
-                                                Ok(value)
-                                            }
-                                            (None, _) => Ok(Value::Null),
-                                        }
-                                    }
-                                    Some(content) => Ok(Value::String(hex_encode(content))),
-                                }
-                            }
-                            Err(err) => Err(format!("Invalid content key requested: {}", err)),
-                        };
-
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::DataRadius => {
-                    let radius = &self.network.overlay.data_radius;
-                    let _ = request.resp.send(Ok(Value::String(radius.to_string())));
-                }
-                StateEndpoint::FindContent => {
-                    let response = match FindContentParams::try_from(request.params) {
-                        Ok(val) => match self
-                            .network
-                            .overlay
-                            .send_find_content(val.enr.into(), val.content_key.into())
-                            .await
-                        {
-                            Ok(content) => match content.try_into() {
-                                Ok(val) => Ok(val),
-                                Err(_) => Err("Content response decoding error".to_string()),
-                            },
-                            Err(msg) => Err(format!("FindContent request timeout: {:?}", msg)),
-                        },
-                        Err(msg) => Err(format!("Invalid FindContent params: {:?}", msg)),
-                    };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::FindNodes => {
-                    let response = match FindNodesParams::try_from(request.params) {
-                        Ok(val) => match self
-                            .network
-                            .overlay
-                            .send_find_nodes(val.enr.into(), val.distances)
-                            .await
-                        {
-                            Ok(nodes) => Ok(nodes.into()),
-                            Err(msg) => Err(format!("FindNodes request timeout: {:?}", msg)),
-                        },
-                        Err(msg) => Err(format!("Invalid FindNodes params: {:?}", msg)),
-                    };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::SendOffer => {
-                    let response = match SendOfferParams::try_from(request.params) {
-                        Ok(val) => {
-                            let content_keys =
-                                val.content_keys.iter().map(|key| key.to_vec()).collect();
-
-                            match self
-                                .network
-                                .overlay
-                                .send_offer(content_keys, val.enr.into())
-                                .await
-                            {
-                                Ok(accept) => Ok(accept.into()),
-                                Err(msg) => Err(format!("Offer request timeout: {:?}", msg)),
-                            }
-                        }
-                        Err(msg) => Err(format!("Invalid Offer params: {:?}", msg)),
-                    };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::Ping => {
-                    let response = match PingParams::try_from(request.params) {
-                        Ok(val) => match self.network.overlay.send_ping(val.enr.into()).await {
-                            Ok(pong) => Ok(pong.into()),
-                            Err(msg) => Err(format!("Ping request timeout: {:?}", msg)),
-                        },
-                        Err(msg) => Err(format!("Invalid Ping params: {:?}", msg)),
-                    };
-                    let _ = request.resp.send(response);
-                }
-                StateEndpoint::RoutingTableInfo => {
-                    let bucket_entries_json =
-                        bucket_entries_to_json(self.network.overlay.bucket_entries());
-
-                    let _ = request.resp.send(Ok(bucket_entries_json));
-                }
-            }
+        while let Some(_request) = self.state_rx.recv().await {
+            warn!("State JSON-RPC handler is not implemented!")
         }
     }
 }
