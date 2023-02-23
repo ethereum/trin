201dba58dcbfa5a86e055d6a7d20c691330d1b01
njgheorghita <nickgheorghita@gmail.com>
2023-02-23 11:06:43 -0500

    Use zero radius if empty storage & 0kb config

--- trin-core/src/portalnet/types/distance.rs
+++ trin-core/src/portalnet/types/distance.rs
@@ -16,6 +16,8 @@ impl fmt::Display for Distance {
 impl Distance {
     /// The maximum value.
     pub const MAX: Self = Self(U256::MAX);
+    /// The minimum value.
+    pub const ZERO: Self = Self(U256::zero());
 
     /// Returns the integer base-2 logarithm of `self`.
     ///
