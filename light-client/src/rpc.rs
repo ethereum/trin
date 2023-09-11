8e97046679b32f419d398d58ac4dd8d60804ceaf
Ognyan Genev <39865181+ogenev@users.noreply.github.com>
2023-09-11 15:16:12 +0300

    refactor(light-client): Strip unnecessary endpoints from the light client node (#899)

--- light-client/src/rpc.rs
+++ light-client/src/rpc.rs
@@ -1,24 +1,17 @@
-use ethers::{
-    abi::AbiEncode,
-    types::{Address, SyncingStatus, U256},
-};
 use eyre::Result;
 use log::info;
-use std::{fmt::Display, net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::RwLock;
 
 use jsonrpsee::{
     core::{async_trait, RpcResult},
     proc_macros::rpc,
     server::{Server, ServerHandle},
-    types::ErrorObject,
     Methods,
 };
 
 use crate::node::Node;
 
-use crate::config::utils::hex_str_to_bytes;
-use crate::types::BlockTag;
 use crate::utils::u64_to_hex_string;
 
 pub struct Rpc {
@@ -53,22 +46,8 @@ impl Rpc {
 
 #[rpc(server, namespace = "eth")]
 trait EthRpc {
-    #[method(name = "getBlockTransactionCountByHash")]
-    async fn get_block_transaction_count_by_hash(&self, hash: &str) -> RpcResult<String>;
-    #[method(name = "getBlockTransactionCountByNumber")]
-    async fn get_block_transaction_count_by_number(&self, block: BlockTag) -> RpcResult<String>;
     #[method(name = "chainId")]
     async fn chain_id(&self) -> RpcResult<String>;
-    #[method(name = "gasPrice")]
-    async fn gas_price(&self) -> RpcResult<String>;
-    #[method(name = "maxPriorityFeePerGas")]
-    async fn max_priority_fee_per_gas(&self) -> RpcResult<String>;
-    #[method(name = "blockNumber")]
-    async fn block_number(&self) -> RpcResult<String>;
-    #[method(name = "getCoinbase")]
-    async fn get_coinbase(&self) -> RpcResult<Address>;
-    #[method(name = "syncing")]
-    async fn syncing(&self) -> RpcResult<SyncingStatus>;
 }
 
 #[rpc(client, server, namespace = "net")]
@@ -85,52 +64,10 @@ struct RpcInner {
 
 #[async_trait]
 impl EthRpcServer for RpcInner {
-    async fn get_block_transaction_count_by_hash(&self, hash: &str) -> RpcResult<String> {
-        let hash = convert_err(hex_str_to_bytes(hash))?;
-        let node = self.node.read().await;
-        let transaction_count = convert_err(node.get_block_transaction_count_by_hash(&hash))?;
-
-        Ok(u64_to_hex_string(transaction_count))
-    }
-
-    async fn get_block_transaction_count_by_number(&self, block: BlockTag) -> RpcResult<String> {
-        let node = self.node.read().await;
-        let transaction_count = convert_err(node.get_block_transaction_count_by_number(block))?;
-        Ok(u64_to_hex_string(transaction_count))
-    }
-
     async fn chain_id(&self) -> RpcResult<String> {
         let node = self.node.read().await;
         let id = node.chain_id();
         Ok(u64_to_hex_string(id))
-    }
-
-    async fn gas_price(&self) -> RpcResult<String> {
-        let node = self.node.read().await;
-        let gas_price = convert_err(node.get_gas_price())?;
-        Ok(format_hex(&gas_price))
-    }
-
-    async fn max_priority_fee_per_gas(&self) -> RpcResult<String> {
-        let node = self.node.read().await;
-        let tip = convert_err(node.get_priority_fee())?;
-        Ok(format_hex(&tip))
-    }
-
-    async fn block_number(&self) -> RpcResult<String> {
-        let node = self.node.read().await;
-        let num = convert_err(node.get_block_number())?;
-        Ok(u64_to_hex_string(num))
-    }
-
-    async fn get_coinbase(&self) -> RpcResult<Address> {
-        let node = self.node.read().await;
-        Ok(node.get_coinbase().unwrap())
-    }
-
-    async fn syncing(&self) -> RpcResult<SyncingStatus> {
-        let node = self.node.read().await;
-        convert_err(node.syncing())
     }
 }
 
@@ -158,30 +95,4 @@ async fn start(rpc: RpcInner) -> Result<(ServerHandle, SocketAddr)> {
     let handle = server.start(methods);
 
     Ok((handle, addr))
-}
-
-fn convert_err<T, E: Display>(res: Result<T, E>) -> RpcResult<T> {
-    // -32099 is a custom error code for a server error
-    // see: https://www.jsonrpc.org/specification#error_object
-    // It's a bit of a cop-out, until we implement more specific errors, being
-    // sure not to confilct with the standard Ethereum error codes:
-    // https://docs.infura.io/networks/ethereum/json-rpc-methods#error-codes
-    res.map_err(|err| ErrorObject::owned(-32099, err.to_string(), None::<()>))
-}
-
-fn format_hex(num: &U256) -> String {
-    let stripped = num
-        .encode_hex()
-        .strip_prefix("0x")
-        .unwrap()
-        .trim_start_matches('0')
-        .to_string();
-
-    let stripped = if stripped.is_empty() {
-        "0".to_string()
-    } else {
-        stripped
-    };
-
-    format!("0x{stripped}")
 }
