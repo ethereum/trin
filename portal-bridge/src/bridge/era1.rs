cb52c9367ba5b2b60200e70417e0099601e81cc2
njgheorghita <nickgheorghita@gmail.com>
2024-02-28 15:41:42 -0500

    fix(bridge): update era1 file source

--- portal-bridge/src/bridge/era1.rs
+++ portal-bridge/src/bridge/era1.rs
@@ -4,9 +4,10 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use anyhow::ensure;
+use anyhow::{anyhow, ensure};
 use futures::future::join_all;
 use rand::{seq::SliceRandom, thread_rng};
+use scraper::{Html, Selector};
 use surf::{Client, Config};
 use tokio::{
     sync::{OwnedSemaphorePermit, Semaphore},
@@ -38,6 +39,7 @@ use trin_validation::{
 };
 
 const ERA1_DIR_URL: &str = "https://era1.ethportal.net/";
+const ERA1_FILE_COUNT: usize = 1897;
 
 pub struct Era1Bridge {
     pub mode: BridgeMode,
@@ -60,21 +62,7 @@ impl Era1Bridge {
             .add_header("Content-Type", "application/xml")
             .expect("to be able to add header")
             .try_into()?;
-        let directory = http_client
-            .get(ERA1_DIR_URL)
-            .recv_string()
-            .await
-            .expect("to be able to read era1 file directory");
-        let xml = roxmltree::Document::parse(&directory)?;
-        let mut era1_files: Vec<String> = xml
-            .descendants()
-            .filter(|n| n.tag_name().name() == "Key")
-            .map(|n| {
-                let key = n.text().expect("to be able to get text");
-                format!("{ERA1_DIR_URL}{key}")
-            })
-            .collect();
-        era1_files.shuffle(&mut thread_rng());
+        let era1_files = get_shuffled_era1_files(&http_client).await?;
         Ok(Self {
             mode,
             portal_clients,
@@ -405,4 +393,35 @@ impl Era1Bridge {
         let content_value = HistoryContentValue::Receipts(block_tuple.receipts.receipts);
         gossip_history_content(&portal_clients, content_key, content_value, block_stats).await
     }
+}
+
+/// Fetches era1 files hosted on era1.ethportal.net and shuffles them
+async fn get_shuffled_era1_files(http_client: &Client) -> anyhow::Result<Vec<String>> {
+    let index_html = http_client
+        .get(ERA1_DIR_URL)
+        .recv_string()
+        .await
+        .map_err(|e| anyhow!("{e}"))?;
+    let index_html = Html::parse_document(&index_html);
+    let selector = Selector::parse("a[href*='mainnet-']").expect("to be able to parse selector");
+    let mut era1_files: Vec<String> = index_html
+        .select(&selector)
+        .map(|element| {
+            let href = element
+                .value()
+                .attr("href")
+                .expect("to be able to get href");
+            format!("{ERA1_DIR_URL}{href}")
+        })
+        .collect();
+    ensure!(
+        era1_files.len() == ERA1_FILE_COUNT,
+        format!(
+            "invalid era1 source, not enough era1 files found: expected {}, found {}",
+            ERA1_FILE_COUNT,
+            era1_files.len()
+        )
+    );
+    era1_files.shuffle(&mut thread_rng());
+    Ok(era1_files)
 }
