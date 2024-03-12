71bd9f49f8f116c8eb57c298fcc5f94a5e30f8be
morph <82043364+morph-dev@users.noreply.github.com>
2024-03-12 15:27:52 +0200

    feat: add state peertests

--- ethportal-peertest/src/utils.rs
+++ ethportal-peertest/src/utils.rs
@@ -1,36 +1,50 @@
+use std::fs;
+
+use futures::{Future, TryFutureExt};
 use tracing::error;
 
 use anyhow::Result;
 use serde_yaml::Value;
-use std::fs;
+use ureq::serde::Deserialize;
 
 use ethportal_api::{
     BeaconContentKey, BeaconContentValue, BeaconNetworkApiClient, HistoryContentKey,
-    HistoryContentValue, HistoryNetworkApiClient,
+    HistoryContentValue, HistoryNetworkApiClient, StateContentKey, StateContentValue,
+    StateNetworkApiClient,
 };
+
+pub async fn wait_for_successful_result<Fut, O>(f: impl Fn() -> Fut) -> O
+where
+    Fut: Future<Output = Result<O>>,
+{
+    // If content is absent an error will be returned.
+    for counter in 0..60 {
+        let result = f().await;
+        match result {
+            Ok(val) => return val,
+            Err(e) => {
+                if counter > 0 {
+                    error!("Retry attempt #{counter} after 0.5s, because we received an error {e}")
+                }
+            }
+        };
+        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+    }
+
+    panic!("Retried too many times");
+}
 
 /// Wait for the history content to be transferred
 pub async fn wait_for_history_content<P: HistoryNetworkApiClient + std::marker::Sync>(
     ipc_client: &P,
     content_key: HistoryContentKey,
 ) -> HistoryContentValue {
-    let mut received_content_value = ipc_client.local_content(content_key.clone()).await;
-
-    let mut counter = 0;
-
-    // If content is absent an error will be returned.
-    while counter < 5 {
-        let message = match received_content_value {
-            Ok(val) => return val,
-            Err(e) => format!("received an error {e}"),
-        };
-        error!("Retrying after 0.5s, because {message}");
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-        received_content_value = ipc_client.local_content(content_key.clone()).await;
-        counter += 1;
-    }
-
-    received_content_value.unwrap()
+    wait_for_successful_result(|| {
+        ipc_client
+            .local_content(content_key.clone())
+            .map_err(anyhow::Error::from)
+    })
+    .await
 }
 
 /// Wait for the beacon content to be transferred
@@ -38,23 +52,25 @@ pub async fn wait_for_beacon_content<P: BeaconNetworkApiClient + std::marker::Sy
     ipc_client: &P,
     content_key: BeaconContentKey,
 ) -> BeaconContentValue {
-    let mut received_content_value = ipc_client.local_content(content_key.clone()).await;
+    wait_for_successful_result(|| {
+        ipc_client
+            .local_content(content_key.clone())
+            .map_err(anyhow::Error::from)
+    })
+    .await
+}
 
-    let mut counter = 0;
-
-    // If content is absent an error will be returned.
-    while counter < 60 {
-        let message = match received_content_value {
-            Ok(val) => return val,
-            Err(e) => format!("received an error {e}"),
-        };
-        counter += 1;
-        error!("Retry attempt #{counter} in 0.5s to find beacon content, because {message}");
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-        received_content_value = ipc_client.local_content(content_key.clone()).await;
-    }
-
-    received_content_value.unwrap()
+/// Wait for the state content to be transferred
+pub async fn wait_for_state_content<P: StateNetworkApiClient + std::marker::Sync>(
+    ipc_client: &P,
+    content_key: StateContentKey,
+) -> StateContentValue {
+    wait_for_successful_result(|| {
+        ipc_client
+            .local_content(content_key.clone())
+            .map_err(anyhow::Error::from)
+    })
+    .await
 }
 
 fn read_history_content_key_value(
@@ -100,4 +116,96 @@ pub fn fixture_block_body() -> (HistoryContentKey, HistoryContentValue) {
 /// Block #14764013 (pre-merge)
 pub fn fixture_receipts() -> (HistoryContentKey, HistoryContentValue) {
     read_fixture("portal-spec-tests/tests/mainnet/history/receipts/14764013.yaml")
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct StateContentData {
+    #[serde(rename = "content_key")]
+    pub key: StateContentKey,
+    #[serde(rename = "content_value_offer")]
+    pub offer_value: StateContentValue,
+    #[serde(rename = "content_value_retrieval")]
+    pub lookup_value: StateContentValue,
+}
+
+#[derive(Debug, Clone)]
+pub struct StateFixture {
+    pub content_data: StateContentData,
+    pub recursive_gossip: Option<StateContentData>,
+}
+
+fn read_state_fixture_from_file(file_name: &str) -> Result<Vec<StateFixture>> {
+    let yaml_content = fs::read_to_string(file_name)?;
+    let value: Value = serde_yaml::from_str(&yaml_content)?;
+
+    let mut result = vec![];
+
+    for fixture in value.as_sequence().unwrap() {
+        result.push(StateFixture {
+            content_data: StateContentData::deserialize(fixture)?,
+            recursive_gossip: fixture.get("recursive_gossip").and_then(|value| {
+                if value.is_null() {
+                    None
+                } else {
+                    Some(StateContentData::deserialize(value).unwrap())
+                }
+            }),
+        });
+    }
+    Ok(result)
+}
+
+fn read_state_fixture(file_name: &str) -> Vec<StateFixture> {
+    read_state_fixture_from_file(file_name)
+        .unwrap_or_else(|err| panic!("Error reading fixture: {err}"))
+}
+
+pub fn fixtures_state_account_trie_node() -> Vec<StateFixture> {
+    read_state_fixture("portal-spec-tests/tests/mainnet/state/validation/account_trie_node.yaml")
+}
+
+pub fn fixtures_state_contract_storage_trie_node() -> Vec<StateFixture> {
+    read_state_fixture(
+        "portal-spec-tests/tests/mainnet/state/validation/contract_storage_trie_node.yaml",
+    )
+}
+
+pub fn fixtures_state_contract_bytecode() -> Vec<StateFixture> {
+    read_state_fixture("portal-spec-tests/tests/mainnet/state/validation/contract_bytecode.yaml")
+}
+
+pub struct StateRecursiveGossipFixture {
+    pub key_value_pairs: Vec<(StateContentKey, StateContentValue)>,
+}
+
+pub fn fixtures_state_recursive_gossip() -> Result<Vec<StateRecursiveGossipFixture>> {
+    let yaml_content = fs::read_to_string(
+        "portal-spec-tests/tests/mainnet/state/validation/recursive_gossip.yaml",
+    )?;
+    let value: Value = serde_yaml::from_str(&yaml_content)?;
+
+    let mut result = vec![];
+
+    for fixture in value.as_sequence().unwrap() {
+        result.push(StateRecursiveGossipFixture {
+            key_value_pairs: fixture
+                .as_sequence()
+                .unwrap()
+                .iter()
+                .map(|key_value_container| {
+                    let key = StateContentKey::deserialize(
+                        key_value_container.get("content_key").unwrap(),
+                    )
+                    .unwrap();
+                    let value = StateContentValue::deserialize(
+                        key_value_container.get("content_value").unwrap(),
+                    )
+                    .unwrap();
+                    (key, value)
+                })
+                .collect(),
+        })
+    }
+
+    Ok(result)
 }
