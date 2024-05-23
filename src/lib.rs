7dd358c9733ec7320f19cd20548cabaaca35b801
njgheorghita <nickgheorghita@gmail.com>
2024-05-23 09:54:33 -0400

    fix: remove newspace from log message

--- src/lib.rs
+++ src/lib.rs
@@ -82,8 +82,10 @@ pub async fn run_trin(
 
     // Initialize validation oracle
     let header_oracle = HeaderOracle::default();
-    info!(hash_tree_root = %hex_encode(header_oracle.header_validator.pre_merge_acc.tree_hash_root().0),"Loaded
-        pre-merge accumulator.");
+    info!(
+        hash_tree_root = %hex_encode(header_oracle.header_validator.pre_merge_acc.tree_hash_root().0),
+        "Loaded pre-merge accumulator."
+    );
     let header_oracle = Arc::new(RwLock::new(header_oracle));
 
     // Initialize state sub-network service and event handlers, if selected
