38101ba8d567f0dc9ab5ba1b6bda419645316501
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-07-03 19:46:41 +0900

    feat: add metrics for Trin Execution (#1335)

--- trin-execution/src/cli.rs
+++ trin-execution/src/cli.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use clap::Parser;
 
 use crate::types::block_to_trace::BlockToTrace;
@@ -18,4 +20,10 @@ pub struct TrinExecutionConfig {
         help = "The block traces will be dumped to the working directory: Configuration options ['none', 'block:<number>', 'all']."
     )]
     pub block_to_trace: BlockToTrace,
+
+    #[arg(
+        long = "enable-metrics-with-url",
+        help = "Enable prometheus metrics reporting (provide local IP/Port from which your Prometheus server is configured to fetch metrics)"
+    )]
+    pub enable_metrics_with_url: Option<SocketAddr>,
 }
