99fabb3cdb43275488c63c0ba0c9f278105de935
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2024-08-05 09:12:50 -0600

    feat: add more metrics to trin execution (#1355)

--- trin-execution/src/metrics.rs
+++ trin-execution/src/metrics.rs
@@ -1,6 +1,17 @@
 use prometheus_exporter::prometheus::{
-    default_registry, register_histogram_vec_with_registry, HistogramTimer, HistogramVec,
+    default_registry, register_histogram_vec_with_registry, register_int_gauge_vec_with_registry,
+    HistogramTimer, HistogramVec, IntGaugeVec,
 };
+
+pub fn create_int_gauge_vec(name: &str, help: &str, label_names: &[&str]) -> IntGaugeVec {
+    let registry = default_registry();
+    register_int_gauge_vec_with_registry!(name, help, label_names, registry)
+        .expect("failed to create int gauge vec")
+}
+
+pub fn set_int_gauge_vec(gauge_vec: &IntGaugeVec, value: i64, label_values: &[&str]) {
+    gauge_vec.with_label_values(label_values).set(value);
+}
 
 pub fn create_histogram_vec(name: &str, help: &str, label_names: &[&str]) -> HistogramVec {
     let registry = default_registry();
@@ -21,5 +32,23 @@ lazy_static::lazy_static! {
         "trin_execution_processing_times",
         "Duration of the sections it takes to execute a block",
         &["section"]
+    );
+
+    pub static ref TRANSACTION_PROCESSING_TIMES: HistogramVec = create_histogram_vec(
+        "trin_execution_transaction_processing_times",
+        "Duration of the sections it takes to execute sections of a transaction",
+        &["section"]
+    );
+
+    pub static ref BUNDLE_COMMIT_PROCESSING_TIMES: HistogramVec = create_histogram_vec(
+        "trin_execution_bundle_commit_processing_times",
+        "Duration of the sections it takes for a bundle to be committed",
+        &["section"]
+    );
+
+    pub static ref BLOCK_HEIGHT: IntGaugeVec = create_int_gauge_vec(
+        "trin_execution_block_height",
+        "The current block height",
+        &[]
     );
 }
