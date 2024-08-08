8395dfa9d03b92d95311ea634ef8d09653b9703f
morph <82043364+morph-dev@users.noreply.github.com>
2024-08-08 17:26:06 +0300

    feat: define state related eth_* rpc endpoints

--- ethportal-api/src/eth.rs
+++ ethportal-api/src/eth.rs
@@ -1,6 +1,6 @@
-use alloy_primitives::{B256, U256};
+use alloy_primitives::{Address, Bytes, B256, U256};
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
-use reth_rpc_types::Block;
+use reth_rpc_types::{Block, BlockId};
 
 /// Web3 JSON-RPC endpoints
 #[rpc(client, server, namespace = "eth")]
@@ -14,4 +14,18 @@ pub trait EthApi {
         block_hash: B256,
         hydrated_transactions: bool,
     ) -> RpcResult<Block>;
+
+    #[method(name = "getBalance")]
+    async fn get_balance(&self, address: Address, block: BlockId) -> RpcResult<U256>;
+
+    #[method(name = "getCode")]
+    async fn get_code(&self, address: Address, block: BlockId) -> RpcResult<Bytes>;
+
+    #[method(name = "getStorageAt")]
+    async fn get_storage_at(
+        &self,
+        address: Address,
+        slot: U256,
+        block: BlockId,
+    ) -> RpcResult<Bytes>;
 }
