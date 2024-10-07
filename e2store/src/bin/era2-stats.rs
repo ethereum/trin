89914a761e6cdcfb58521338ab666d5677c93c22
Milos Stankovic <82043364+morph-dev@users.noreply.github.com>
2024-10-07 12:57:49 +0300

    feat: add binary for simple era2 analysis

---
+++ e2store/src/bin/era2-stats.rs
@@ -0,0 +1,72 @@
+use std::path::PathBuf;
+
+use anyhow::bail;
+use clap::Parser;
+use e2store::era2::{AccountOrStorageEntry, Era2Reader};
+use tracing::info;
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "Era2 stats",
+    about = "Reads the era2 file and prints stats about it."
+)]
+struct Config {
+    #[arg(help = "The path to the era2 file.")]
+    path: PathBuf,
+}
+
+#[derive(Debug, Default)]
+struct Stats {
+    account_count: usize,
+    bytecode_count: usize,
+    storage_entry_count: usize,
+    storage_item_count: usize,
+}
+
+/// Reads the era2 file and prints stats about it.
+///
+/// It can be run with following command:
+/// ```
+/// cargo run -p e2store --bin era2-stats --features era2-stats-binary -- <path>
+/// ```
+fn main() -> anyhow::Result<()> {
+    trin_utils::log::init_tracing_logger();
+
+    let config = Config::parse();
+    let mut reader = Era2Reader::open(&config.path)?;
+
+    info!(
+        "Processing era2 file for block: {}",
+        reader.header.header.number
+    );
+
+    let mut stats = Stats::default();
+    while let Some(entry) = reader.next() {
+        let AccountOrStorageEntry::Account(account) = entry else {
+            bail!("Expected account entry");
+        };
+        stats.account_count += 1;
+
+        if !account.bytecode.is_empty() {
+            stats.bytecode_count += 1;
+        }
+
+        for _ in 0..account.storage_count {
+            let Some(AccountOrStorageEntry::Storage(storage)) = reader.next() else {
+                bail!(
+                    "Expected storage entry for account: {}",
+                    account.address_hash
+                );
+            };
+            stats.storage_entry_count += 1;
+            stats.storage_item_count += storage.len();
+        }
+
+        if stats.account_count % 1_000_000 == 0 {
+            info!("Processed {} accounts", stats.account_count);
+        }
+    }
+
+    info!("Done! {stats:#?}");
+    Ok(())
+}
