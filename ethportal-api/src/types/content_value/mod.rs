65dd71ff29b3335d4529720aeb344b528c5911ac
Milos Stankovic <82043364+morph-dev@users.noreply.github.com>
2024-10-20 18:29:36 +0300

    docs: update RawContentKey and RawContentValue docs

--- ethportal-api/src/types/content_value/mod.rs
+++ ethportal-api/src/types/content_value/mod.rs
@@ -15,7 +15,12 @@ pub trait ContentValue: Sized {
     type TContentKey: OverlayContentKey;
 
     /// Encodes the content value into a byte vector.
+    ///
+    /// The [RawContentValue] is better suited than `Vec<u8>` for representing content value bytes.
+    /// For more details, see [RawContentValue] documentation. If `Vec<u8>` is still desired, one
+    /// can obtain it with: `value.to_bytes().to_vec()`.
     fn encode(&self) -> RawContentValue;
+
     /// Decodes `buf` into a content value.
     fn decode(key: &Self::TContentKey, buf: &[u8]) -> Result<Self, ContentValueError>;
 
@@ -23,6 +28,7 @@ pub trait ContentValue: Sized {
     fn to_hex(&self) -> String {
         hex_encode(self.encode())
     }
+
     /// Decodes the "0x"-prefixed hex string as a content value.
     fn from_hex(key: &Self::TContentKey, data: &str) -> anyhow::Result<Self> {
         Ok(Self::decode(key, &hex_decode(data)?)?)
