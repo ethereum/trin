0b54f248976ef8d9112588b2cc9c9d882f522199
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-01-17 14:11:44 -0700

    fix: cross build ci required for releases + remove const_format (#1643)

--- crates/ethportal-api/src/version.rs
+++ crates/ethportal-api/src/version.rs
@@ -1,29 +1,22 @@
+use std::env;
+
 /// The latest git commit hash of the build.
 pub const TRIN_FULL_COMMIT: &str = env!("VERGEN_GIT_SHA");
-pub const TRIN_SHORT_COMMIT: &str = const_format::str_index!(TRIN_FULL_COMMIT, ..8);
+pub const TRIN_SHORT_COMMIT: &str = env!("VERGEN_GIT_SHA_SHORT");
 
 /// Trin's version is the same as the git tag.
-pub const TRIN_VERSION: &str = const_format::str_split!(env!("VERGEN_GIT_DESCRIBE"), '-')[0];
+pub const TRIN_VERSION: &str = env!("TRIN_VERSION");
 
 /// The operating system of the build, linux, macos, windows etc.
-pub const BUILD_OPERATING_SYSTEM: &str =
-    const_format::str_split!(env!("VERGEN_CARGO_TARGET_TRIPLE"), "-")[2];
+pub const BUILD_OPERATING_SYSTEM: &str = env!("TRIN_BUILD_OPERATING_SYSTEM");
 
 /// The architecture of the build, x86_64, aarch64, etc.
-pub const BUILD_ARCHITECTURE: &str =
-    const_format::str_split!(env!("VERGEN_CARGO_TARGET_TRIPLE"), "-")[0];
+pub const BUILD_ARCHITECTURE: &str = env!("TRIN_BUILD_ARCHITECTURE");
 
 // /// The version of the programming language used to build the binary.
 pub const PROGRAMMING_LANGUAGE_VERSION: &str = env!("VERGEN_RUSTC_SEMVER");
 
-pub const FULL_VERSION: &str = const_format::formatcp!(
-    "{version}-{hash} {build_os}-{build_arch} rustc{rust_version}",
-    version = TRIN_VERSION,
-    hash = TRIN_SHORT_COMMIT,
-    build_os = BUILD_OPERATING_SYSTEM,
-    build_arch = BUILD_ARCHITECTURE,
-    rust_version = PROGRAMMING_LANGUAGE_VERSION
-);
+pub const FULL_VERSION: &str = env!("TRIN_FULL_VERSION");
 
 /// Returns the trin version and git revision.
 pub const fn get_trin_version() -> &'static str {
