172135a92cfb9ade20a3d55de01d551fdefc64d3
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2025-02-13 10:36:33 -0800

    fix: remove unused code and alphebatize light client cargo toml

--- crates/light-client/src/utils.rs
+++ crates/light-client/src/utils.rs
@@ -1,11 +1,3 @@
-// pub fn bytes_to_bytes32(bytes: &[u8]) -> Bytes32 {
-//     FixedVector::from(bytes.to_vec())
-// }
-
-// pub fn bytes32_to_node(bytes: &Bytes32) -> Result<B256> {
-//     Ok(B256::from_slice(bytes.iter().as_slice()))
-// }
-
 pub fn u64_to_hex_string(val: u64) -> String {
     format!("0x{val:x}")
 }
