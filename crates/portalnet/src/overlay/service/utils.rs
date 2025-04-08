56b016bf8f00fae206c92d92d6f13789fa530a17
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2025-04-07 18:34:49 -0600

    fix: address PR feedback

--- crates/portalnet/src/overlay/service/utils.rs
+++ crates/portalnet/src/overlay/service/utils.rs
@@ -1,9 +1,11 @@
-use ethportal_api::SszEnr;
 use ssz::Encode;
 
-/// Limits a to a maximum packet size, including the discv5 header overhead.
-pub fn pop_while_ssz_bytes_len_gt(enrs: &mut Vec<SszEnr>, max_size: usize) {
-    while enrs.ssz_bytes_len() > max_size {
-        enrs.pop();
+/// Limits the elements count to a maximum packet size, including the discv5 header overhead.
+pub fn pop_while_ssz_bytes_len_gt<SSZObject: Encode>(
+    elements: &mut Vec<SSZObject>,
+    max_size: usize,
+) {
+    while elements.ssz_bytes_len() > max_size {
+        elements.pop();
     }
 }
